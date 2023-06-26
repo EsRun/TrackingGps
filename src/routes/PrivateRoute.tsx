75d4361dee35da2ebac9200c517f668aeeadee20
비엔나 <sure@paran.com>
2023-06-26 15:51:03 +0900

    fix : PrivateRoute

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { Component } from "react";
+import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ authenticated: boolean, component:Component}) => {
-
-    return(
-        authenticated?Component:<Navigate to="/login" {...alert("로그인 해")}></Navigate>
-    )
+interface PrivateProps {
+  authenticated: boolean;
+  component: React.ReactNode | null;
 }
+
+const PrivateRoute: React.FC<PrivateProps> = ({ authenticated, component }) => {
+  return authenticated ? component : <Navigate to="/login"></Navigate>;
+};
 
 export default PrivateRoute;
