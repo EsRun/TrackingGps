80c71c4ab3825f55aabebcff994905697f206d3a
비엔나 <sure@paran.com>
2023-07-07 21:01:34 +0900

    ...

--- src/pages/main/Text.tsx
+++ src/pages/main/Text.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Text = () => {
   return (
     <div style={{ width: "100%", height: "100%", backgroundColor: "#ccc" }}>
-      ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
+      테스트 페이지
     </div>
   );
 };
