4c57fa2c5fdde6b23cc24da90302a13b0c6a85d0
비엔나 <sure@paran.com>
2023-07-17 16:05:58 +0900

    add : test child component

--- src/layout/Side.tsx
+++ src/layout/Side.tsx
@@ -5,9 +5,14 @@ import React from "react";
 interface sideComponenetProps {
   title: string;
   link: string;
+  component?: JSX.Element;
 }
 
-const SideComponent: React.FC<sideComponenetProps> = ({ title, link }) => {
+const SideComponent: React.FC<sideComponenetProps> = ({
+  title,
+  link,
+  component,
+}) => {
   return (
     <Box
       sx={{
@@ -26,10 +31,21 @@ const SideComponent: React.FC<sideComponenetProps> = ({ title, link }) => {
         {title}
       </Typography>
       <Link href={link}>{link}</Link>
-      <Box height={200}>
-        <Select><option selected>1</option></Select>
-      </Box>
+      <Box height={200}>{component}</Box>
     </Box>
+  );
+};
+
+const InComponent = (): JSX.Element => {
+  return (
+    <>
+      <Select>
+        <option>10</option>
+        <option>30</option>
+        <option>50</option>
+        <option>100</option>
+      </Select>
+    </>
   );
 };
 
@@ -44,7 +60,11 @@ const Side = () => {
           width: "200px",
         }}
       >
-        <SideComponent title="Compoenet1" link="/s" />
+        <SideComponent
+          title="Compoenet1"
+          link="/s"
+          component={<InComponent />}
+        />
         <SideComponent title="Compoenet2" link="/s" />
         <SideComponent title="Compoenet3" link="/s" />
       </Container>
