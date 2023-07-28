c72a66cd00a87c572b58ebca46f184ad6e238174
비엔나 <sure@paran.com>
2023-07-28 14:48:31 +0900

    test : outlet context

--- src/pages/main/Text.tsx
+++ src/pages/main/Text.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
+interface UserInfoConText {
+  currentSelect: string;
+}
 
 const Text = () => {
+  const { currentSelect } = useOutletContext<UserInfoConText>();
   return (
     <div style={{ width: "100%", height: "100%", backgroundColor: "#ccc" }}>
       테스트 페이지
+      {currentSelect}
     </div>
   );
 };
