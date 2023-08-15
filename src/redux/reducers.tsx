ac402d15c6e806494d24518103564d3102eeafbc
비엔나 <sure@paran.com>
2023-08-16 07:33:26 +0900

    fix : reducer

--- src/redux/reducers.tsx
+++ src/redux/reducers.tsx
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export const circleSlice = createSlice({
-  name: "counter",
+  name: "circle",
   initialState: { value: 1000 },
   reducers: {
     circleRadius: (state, action) => {
